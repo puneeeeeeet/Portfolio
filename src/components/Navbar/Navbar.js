b51e2386ad8ce8afb6cf0a30e7ae82acfd258298
puneeeeeeet <puneetchhabra1607@gmail.com>
2025-04-08 13:38:11 +0530

    document path corrected

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,10 @@
-import resume from '../../documents/Document.pdf'
 const NavBar = () => {
     return (
       <nav className="p-[20px] flex  items-center max-w-[800px] m-auto border-b-2 border-white o">
         <h1 className="text-white font-bold text-2xl">Puneet Chhabra</h1>
         <div className="  ml-auto z-10"> 
           <div className="ml-16 no-underline space-x-3 text-red-600">
-            <a href={resume} target="_blank"   >Resume</a>
+            <a href="/documents/Document.pdf" target="_blank"   >Resume</a>
            
           </div>
         </div>
